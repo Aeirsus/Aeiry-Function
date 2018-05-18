dc7efe47c1e091a6e1587d546a1ac3aaed083dc5
Aeirsus <39391142+Aeirsus@users.noreply.github.com>
2018-05-17 17:30:50 -0700

    Add files via upload

---
+++ test - Copy.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<!-- Aeiry Function website -->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aeiry Function</title>
+	<!-- Google Analytics -->
+	<!-- Metadata -->
+	<meta name="author" content="Aeirsus">
+	<meta name="description" content="Aeiry Function is a sole proprietorship which aims to provide art, animation, music, and games.">
+	<meta name="viewport" content="width=device-width">
+		
+	<!-- CSS -->
+	<!-- Javascript -->
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div id="topbar">
+				<div id="logo">
+					
+				</div>
+				<div class="logo-extension">
+						
+				</div>
+				<div id="follow-me">
+					<!-- Facebook, Tumblr, Twitter, Deviantart, Pixiv, Youtube /pick 4 -->
+					<!-- <table class="icon-links">
+						<tbody>
+							<tr colspan="2">
+								<h5>Links</h5>
+							</tr>	
+							<tr>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table> -->
+				</div>
+			</div>
+			<div id="navigation">
+				<div id="logo-name">
+					
+				</div>
+				<div id="logo-spacer">
+					<!-- horizontal spacer -->
+				</div>
+				<nav>
+					<ul>
+						<li>
+							<!-- Home -->
+							<p>Home</p>
+						</li>
+						<li>
+							<!-- Info
+							About, Contact, Links -->
+							<p>Info</p>
+						</li>
+						<li>
+							<!-- Blog -->
+							<p>Blog</p>
+						</li>
+						<li>
+							<!-- Art
+							2D and 3D -->
+							<p>Art</p>
+						</li>
+						<li>
+							<!-- Animation
+							2D and 3D -->
+							<p>Animation</p>
+						</li>
+						<li>
+							<!-- Music -->
+							<p>Music</p>
+						</li>
+						<li>
+							<!-- Projects
+							Doujinshi, Projekt Connect-->
+							<p>Projects</p>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div id="nav-bottom-border">
+				
+			</div>
+		</header>
+		<hr>
+		<div class="mainContent">
+			
+		</div>
+		<footer>
+			<div id="copyright">
+				
+			</div>
+		</footer>
+	</div>
+	
+	<a href="_img/Lavender-Ice Pirate Fox.jpg" title="Pirate Fox"><img src="_img/Lavender-Ice Pirate Fox.jpg" width="200"></a></p>
+</body>
+</html>
